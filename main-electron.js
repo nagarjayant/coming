1ba25efc6fa05a1377db74116613794e135b30a8
nagarjayant <31368599+nagarjayant@users.noreply.github.com>
2025-07-11 17:56:06 +0530

    Add files via upload

---
+++ main-electron.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
